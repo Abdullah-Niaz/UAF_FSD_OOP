92d28f6b20782a0fea7b99dbd7689e5dd65ca7f7
Abdullah-Niaz <abdullahniaz423@gmail.com>
2025-11-07 14:28:16 +0500

    pointer with array

--- code/Pointer_with_Array/pointer_with_arry.cpp
+++ code/Pointer_with_Array/pointer_with_arry.cpp
@@ -4,9 +4,18 @@ using namespace std;
 int main()
 {
     int intarray[5] = {31, 54, 77, 52, 93};
+    cout << "Acessing with constant" << endl;
     for (int j = 0; j < 5; j++)
+    {
         cout << *(intarray + j) << endl;
+    }
+
+    int *parray = intarray;
+    cout << "Acessing with pointer" << endl;
+    for (int j = 0; j < 5; j++)
+    {
+        cout << *(parray++) << endl;
+    }
 
     return 0;
 }
-
