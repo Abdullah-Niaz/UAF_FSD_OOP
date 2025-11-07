28df03714f2b3370f7c130a9bab594c230de6e07
Abdullah-Niaz <abdullahniaz423@gmail.com>
2025-11-07 13:58:55 +0500

    pass by value

---
+++ code/PointerFunction.cpp/01_Pass_By_Value.cpp
@@ -0,0 +1,26 @@
+// In C++, there are three main ways to pass arguments to a function :
+// 1. By Value
+// 2. By Reference
+// 3. By Pointer
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void swapVariable(int a, int b);
+void changeValue(int x);
+
+int main()
+{
+    int num = 10;
+    cout << "Before the Function Call: " << num << endl;
+    changeValue(num);
+    cout << "After the Function Call: " << num << endl;
+
+    return 0;
+}
+
+void changeValue(int x)
+{
+    x = 50;
+    cout << "In Function: " << x << endl;
+}
